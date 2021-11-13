ccc10e56a55e6eddb53babec0e583b90631f9def
lmwamba-hai <bmwamba@stedwards.edu>
2021-11-13 15:36:49 -0600

    login.php md5 test 6

--- login.php
+++ login.php
@@ -40,6 +40,7 @@
         echo "md5(password): " . $md5_pass . "<br>";
 
         $sql = "SELECT * FROM UserAccounts WHERE username='$user_name' AND password='$pass'";
+        echo $sql."<br>";
         $result = $conn -> query($sql);
         if (mysqli_num_rows($result) < 1) {
             $row = mysqli_fetch_assoc($result);
