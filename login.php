599bae51572a5390aa02e57b6aa405d72b1a18e3
lmwamba-hai <bmwamba@stedwards.edu>
2021-11-12 23:04:38 -0600

    login.php clearance test 6

--- login.php
+++ login.php
@@ -43,10 +43,35 @@
             $row = mysqli_fetch_assoc($result);
 
             if ($row['username'] === $user_name && $row['password'] === $pass) {
-                echo "Logged in!<br>";
+                echo "<h2>Welcome you</h2><br>";
                 $_SESSION['username'] = $row['username'];
                 $_SESSION['clearance'] = $row['clearance'];
 
+                if ($_SESSION['clearance'] === "T"){
+                    $images = array("TopSecret.png","Secret.png","Confidential.png","Unclassified.png")
+                    foreach ($images as $image) {
+                        echo '<div class="center"><img src="$image"></div>'
+                    }
+                }
+                if ($_SESSION['clearance'] === "S"){
+                    $images = array("Secret.png","Confidential.png","Unclassified.png")
+                    foreach ($images as $image) {
+                        echo '<div class="center"><img src="$image"></div>'
+                    }
+                }
+                exit();
+                if ($_SESSION['clearance'] === "C"){
+                    $images = array("Confidential.png","Unclassified.png")
+                    foreach ($images as $image) {
+                        echo '<div class="center"><img src="$image"></div>'
+                    }
+                }
+                if ($_SESSION['clearance'] === "U"){
+                    $images = array("Unclassified.png")
+                    foreach ($images as $image) {
+                        echo '<div class="center"><img src="$image"></div>'
+                    }
+                }
                 exit();
             }
             else{
@@ -60,8 +85,7 @@
         }
         echo $_SESSION['clearance']."<br>";
         ?>
-        <h2>Welcome you</h2>
-        <br>
+
 
     </div>
     <script src="script.js"></script>
