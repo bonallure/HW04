e456a75964169a29780ea4050eea92353f9a2959
lmwamba-hai <bmwamba@stedwards.edu>
2021-11-12 21:30:00 -0600

    login.php login test 7

--- login.php
+++ login.php
@@ -18,7 +18,7 @@ function validate($data){
            $data = trim($data);
 
            return $data;
-
+}
 $user_name = validate($_POST['username']);
 echo "username: ".$user_name."<br>";
 $pass = validate($_POST['password']);
@@ -28,3 +28,56 @@ $sql = "SELECT * FROM UserAccounts WHERE username='$user_name' AND password='$pa
 $result = mysqli_query($conn, $sql);
 echo $result . "<br>";
 ?>
+/*
+
+    session_start();
+
+
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+
+        }
+
+        $user_name = validate($_POST['username']);
+        $pass = validate($_POST['password']);
+
+        if (empty($user_name)) {
+            header("Location: index.html?error=User Name is required");
+            exit();
+        }
+        else if(empty($pass)){
+            header("Location: index.php?error=Password is required");
+            exit();
+        }
+        else{
+            $sql = "SELECT * FROM UserAccounts WHERE username='user_name' AND password='$pass'";
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) === 1) {
+                $row = mysqli_fetch_assoc($result);
+
+                if ($row['username'] === $user_name && $row['password'] === $pass) {
+                    echo "Logged in!";
+                    $_SESSION['username'] = $row['user_name'];
+                    $_SESSION['userID'] = $row['id'];
+                    $_SESSION['clearance'] = $row['clearance'];
+                    header("Location: home.php");
+
+                    exit();
+                }
+                else{
+                    header("Location: index.php?error=Incorect User name or password");
+                    exit();
+                }
+            }
+            else{
+                header("Location: index.php?error=Incorect User name or password");
+                exit();
+            }
+        }
+
+    }
+    else{
+        header("Location: index.php");
+        exit();
+    }
+*/
